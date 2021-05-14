1a685c8843419dca9720f2489a78e3d8009574e9
swarnadeep721 <77247694+swarnadeep721@users.noreply.github.com>
2021-05-14 17:53:30 +0530

    Add files via upload
    Here is my react.JS app which filters the friend cards by searching them by name.

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'utkarsh',
+    username: 'ut',
+    email: 'ut@april.biz'
+  },
+  {
+    id: 2,
+    name: 'priyam',
+    username: 'pri',
+    email: 'pri@melissa.tv'
+  },
+  {
+    id: 3,
+    name: 'arjun',
+    username: 'ar',
+    email: 'ar@yesenia.net'
+  },
+  {
+    id: 4,
+    name: 'Swarnadeep',
+    username: 'ta',
+    email: 'ta@kory.org'
+  },
+  {
+    id: 5,
+    name: 'sankalpo',
+    username: 'rik',
+    email: 'rik@annie.ca'
+  },
+  {
+    id: 6,
+    name: 'Mrs. Dennis ',
+    username: 'Leopoldo_Corkery',
+    email: 'Karley_Dach@jasper.info'
+  },
+  {
+    id: 7,
+    name: 'Kurtis ',
+    username: 'Elwyn.Skiles',
+    email: 'Telly.Hoeger@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'Nicholas ',
+    username: 'Maxime_Nienow',
+    email: 'Sherwood@rosamond.me'
+  },
+  {
+    id: 9,
+    name: 'Glenna ',
+    username: 'Delphine',
+    email: 'Chaim_McDermott@dana.io'
+  },
+  {
+    id: 10,
+    name: 'Clementina ',
+    username: 'Moriah.Stanton',
+    email: 'Rey.Padberg@karina.biz'
+  }
+];
